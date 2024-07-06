a003f086dcf2d91eab6b33e4556fb9afbef247a2
Cinthia M. Orona <devrider2021@gmail.com>
2024-07-06 20:53:40 -0300

    termine maquetado de los login

--- frontend/agendapp/src/app/components/login/login.component.ts
+++ frontend/agendapp/src/app/components/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NavbarComponent, RouterLink],
+  imports: [NavbarComponent, RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
@@ -13,19 +14,33 @@ export class LoginComponent implements OnInit {
   vistaHeader = true; //para ver el header condicionalmente
   role: string = '';
 
+  user: any = {
+    email: '',   
+    contrasenia: ''   
+  };
 
   constructor(private route: ActivatedRoute, private router: Router) {
     this.role = this.route.snapshot.routeConfig?.path || '';
-  
   }
 
   ngOnInit(): void {
     console.log(this.role);
   }
 
-  irHacia(role: string){
+  irHacia(role: string) {
     this.role = role;
-    window.location
+    window.location;
   }
 
+  verDatos(form: NgForm) {
+    if (form.valid) {
+      const datos = form.value;
+      confirm(
+        `Email: ${datos.email} \nContraseña: ${datos.contrasenia}`
+      );
+      form.reset();
+    } else {
+      alert('Por favor, completa todos los campos.');
+    }
+  }
 }
