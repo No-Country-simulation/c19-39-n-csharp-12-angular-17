f9d1a9a54cfa9084577ab034abba51efc331a14e
Cinthia M. Orona <devrider2021@gmail.com>
2024-07-17 12:26:13 -0300

    modifiqué videollamada y icono footer

--- frontend/agendapp/src/app/components/videollamada/videollamada.component.ts
+++ frontend/agendapp/src/app/components/videollamada/videollamada.component.ts
@@ -1,6 +1,14 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-videollamada',
@@ -9,46 +17,104 @@ import { FooterComponent } from '../../shared/footer/footer.component';
   templateUrl: './videollamada.component.html',
   styleUrl: './videollamada.component.css',
 })
-export class VideollamadaComponent implements AfterViewInit {
+export class VideollamadaComponent implements OnInit, OnDestroy {
   @ViewChild('video')
   public video!: ElementRef<HTMLVideoElement>;
   error: any;
+  private stream!: MediaStream;
 
-  async ngAfterViewInit() {
-    await this.setupDevices();
-  }
-
-  async setupDevices() {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  async ngOnInit() {
+    if (navigator.mediaDevices.getUserMedia) {
       try {
-        const stream = await navigator.mediaDevices.getUserMedia({
+        this.stream = await navigator.mediaDevices.getUserMedia({
           video: true,
         });
-        if (stream) {
-          if (this.video) {
-            this.video.nativeElement.srcObject = stream;
-            this.video.nativeElement.play();
-            this.error = null;
-          }
-        } else {
-          this.error = 'You have no output video device';
+
+        if (this.stream) {
+          this.video.nativeElement.srcObject = this.stream;
+          this.video.nativeElement.play();
         }
       } catch (e) {
-        this.error = e;
+        console.error('Error al acceder a la cámara: ', e);
       }
+    } else {
+      console.error('El navegador no soporta getUserMedia');
     }
   }
 
-  onPlay() {
-    if (this.video) {
+  pauseStream(){
+    if(this.stream){
+     this.video.nativeElement.pause();
+    } else {
+      console.error('No hay stream para pausar');
+    }
+  }
+
+  playStream(){
+    if(this.stream){
       this.video.nativeElement.play();
+    } else {
+      console.error('No hay stream para reproducir');
     }
   }
 
-  onPause() {
-    if (this.video) {
-      this.video.nativeElement.pause();
+  stopStream() {
+    if (this.stream) {
+      const tracks = this.stream.getTracks();
+      tracks.forEach((track) => track.stop());
+      this.video.nativeElement.srcObject = null;
+    } else {
+      console.error('No hay stream para detener');
     }
   }
 
+  ngOnDestroy() {
+    this.stopStream();
+  }
+
+  // async ngAfterViewInit() {
+  //   await this.setupDevices();
+  // }
+
+  // constructor(private router: Router) {}
+
+  // async setupDevices() {
+  //   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  //     try {
+  //       const stream = await navigator.mediaDevices.getUserMedia({
+  //         video: true,
+  //       });
+  //       if (stream) {
+  //         if (this.video) {
+  //           this.video.nativeElement.srcObject = stream;
+  //           this.video.nativeElement.play();
+  //           this.error = null;
+  //         }
+  //       } else {
+  //         this.error = 'No tienes dispositivo de salida de video';
+  //         console.log('No tienes dispositivo de salida de video');
+  //       }
+  //     } catch (e) {
+  //       this.error = e;
+  //       console.log('Mostrar errores: ', e);
+  //     }
+  //   }
+  // }
+
+  // onPlay() {
+  //   if (this.video) {
+  //     this.video.nativeElement.play();
+  //     console.log('Video reproduciendose');
+  //   }
+  // }
+
+  // onPause() {
+  //   if (this.video) {
+  //     this.video.nativeElement.pause();
+
+  //     console.log('Video pausado');
+  //   }
+  // }
+
+  // ngOnDestroy(): void {}
 }
